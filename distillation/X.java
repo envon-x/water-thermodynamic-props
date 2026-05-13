1c024ce30c12fcebe9ee6ab4e2146ed709c09d49
Raul <hrc.bon@gmail.com>
2026-05-13 10:39:29 -0400

    Se ha separado clases relacionadas a operacines unitarias de destilación

--- distillation/X.java
+++
@@ -1,9 +0,0 @@
-package com.iridiscense.unitoperations.distillation;
-
-/**
- * This class was created for Comanda project
- * Created by bon on 3/22/21.
- * Copyright (c)  Hector Bonifacio. 3/22/21, All rights reserved.
- */
-public class X {
-}
